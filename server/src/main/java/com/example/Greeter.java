ba478c5f85f5ea03d30abf8e2fae3f99d9dc53e8
Tom Atkinson <tom.atkinson@cloudreach.com>
2018-10-30 14:23:40 +0000

    fixed errors

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,14 @@ public class Greeter {
   public Greeter() {
 
   }
+  
+  /** 
+  * @param someone the name of this person 
+  * @ return greeting string 
+  */ 
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+ 
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
